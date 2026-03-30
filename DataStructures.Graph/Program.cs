5b606cb71bfb2c5e6292b0acd7fc5324a034fe9f
Igor Santana Medeiros <1434@adm.ucdb.br>
2026-03-30 14:38:13 -0400

    (feat): Added topological sort

--- DataStructures.Graph/Program.cs
+++ DataStructures.Graph/Program.cs
@@ -1,2 +1,17 @@
-﻿// var graph = new Graph();
+﻿using DataStructures.Graph;
+
+var graph = new Graph(9, true);
+graph.AddEdge(0, 1);
+graph.AddEdge(1, 2);
+graph.AddEdge(2, 7);
+graph.AddEdge(2, 4);
+graph.AddEdge(2, 3);
+graph.AddEdge(1, 5);
+graph.AddEdge(5, 6);
+graph.AddEdge(3, 6);
+graph.AddEdge(3, 4);
+graph.AddEdge(6, 8);
+
+var topologicalSort = graph.TopologicalSort();
+Console.WriteLine();
 
